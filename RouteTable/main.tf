5b0fe3f5e4df48c9c154b0b49e680dd68424a5a3
Your Name <you@example.com>
2026-08-19 13:02:18 +0000

    first-commit1

---
+++ RouteTable/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_route_table" "route_table" {
+  name                = var.route_table_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  route {
+    name                   = "default-route"
+    address_prefix         = "0.0.0.0/0"
+    next_hop_type          = "Internet"
+  }
+}
