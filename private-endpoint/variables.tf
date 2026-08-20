3961b8e4db2a78e1636a09da7bf2be2fd3844658
RammohanSOC <abburirammohan2@gmail.com>
2026-08-20 15:12:37 +0530

    Add variables for private endpoint configuration

---
+++ private-endpoint/variables.tf
@@ -0,0 +1,23 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "private_endpoint_name" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "target_resource_id" {
+  type = string
+}
+
+variable "subresource_names" {
+  type = list(string)
+}
