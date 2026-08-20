c435667fc7610012478d442620121105df6078ed
RammohanSOC <abburirammohan2@gmail.com>
2026-08-20 14:59:06 +0530

    Add variables for VPN Gateway configuration

---
+++ VPN-Gateway/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "vpn_gateway_name" {
+  type = string
+}
+
+variable "gateway_subnet_id" {
+  type = string
+}
+
+variable "public_ip_id" {
+  type = string
+}
