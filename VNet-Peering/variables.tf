fb2edc79189f17e47c217e79f1bf4634c6545bc3
RammohanSOC <abburirammohan2@gmail.com>
2026-08-20 15:20:32 +0530

    Add variables for VNet peering configuration

---
+++ VNet-Peering/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "remote_vnet_id" {
+  type = string
+}
+
+variable "peering_name" {
+  type = string
+}
